ccd26cc865c401e385016d78c0df575a99f763c0
tinanano19 <37637107+tinanano19@users.noreply.github.com>
2021-08-08 17:25:49 -0500

    Menu in game

---
+++ Assets/Scripts/MenuInGame.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MenuInGame : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        Cursor.visible = false;
+        pauseMenuUI.SetActive(false);
+        GameIsPaused = false;
+        Time.timeScale = 1f;
+    }
+    void Pause()
+    {
+        Cursor.visible = true;
+        pauseMenuUI.SetActive(true);
+        GameIsPaused = true;
+        Time.timeScale = 0f;
+    }
+
+    public void Quit()
+    {
+        SceneManager.LoadScene(3);
+    }
+}
