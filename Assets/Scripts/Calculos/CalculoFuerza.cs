3682e03eace7d3812153f02a00f0d6d07a1a397b
Sagalo04 <santiago.garcna@uao.edu.co>
2021-09-07 19:42:43 -0500

    Fneta

---
+++ Assets/Scripts/Calculos/CalculoFuerza.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CalculoFuerza : MonoBehaviour
+{
+    public TMP_InputField inputmass1;
+    public TMP_InputField inputmass2;
+    public TMP_InputField inputmass3;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnclickedForce()
+    {
+        float F1 = float.Parse(inputmass1.text);
+        bool A1 = F1 >= 154.2 && F1 <= 154.4;
+
+        float F2 = float.Parse(inputmass2.text);
+        bool A2 = F2 >= 101.1 && F2 <= 101.3;
+
+        float F3 = float.Parse(inputmass3.text);
+        bool A3 = F3 >= 49.7 && F3 <= 49.9;
+
+        Debug.Log(A1);
+        Debug.Log(A2);
+        Debug.Log(A3);
+
+        if (A1 && A2 && A3) {
+            Debug.Log("CORRECTO");
+        }
+        else
+        {
+            Debug.Log("INCORRECTO");
+        }
+    }
+}
