b756c7eeea33e7507cdfce4cbf6529e7b8217220
tinanano19 <37637107+tinanano19@users.noreply.github.com>
2021-09-12 18:42:43 -0500

    Finales incorrectos

--- Assets/Scripts/FinalIncorrecto.cs
+++ Assets/Scripts/FinalIncorrecto.cs
@@ -7,12 +7,19 @@ public class FinalIncorrecto : MonoBehaviour
     public GameObject pantalla;
     public GameObject activador;
     public GameObject sonido;
-    // Start is called before the first frame update
-    public void OnTriggerEnter(Collider other)
+
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+
+        // Start is called before the first frame update
+        public void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Bomba"))
         {
             pantalla.SetActive(true);
+            Cursor.visible = true;
             sonido.SetActive(true);
             activador.SetActive(false);
 
